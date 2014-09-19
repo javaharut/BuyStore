ae09fb43752cedc65fa50c240aee5aeec894e895
Arman <zetbrush@gmail.com4>
2014-09-19 11:47:11 +0400

    arman

--- BuyStore/src/main/Main.java
+++ BuyStore/src/main/Main.java
@@ -1,7 +1,13 @@
 package main;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    List<Products> products = new ArrayList<Products>();
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
---
+++ BuyStore/src/main/Products.java
@@ -0,0 +1,49 @@
+package main;
+
+/**
+ * Created by Arman on 9/19/2014.
+ */
+public class Products {
+    String name;
+    String description;
+    int count;
+    int price;
+
+
+   public Products(String name, String description, int count, int price)
+   {
+       this.name=name;
+       this.description=description;
+       this.count=count;
+       this.price =price;
+
+   }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public int getCount(){
+        return count;
+    }
+    public int getPrice(){
+        return price;
+    }
+}
