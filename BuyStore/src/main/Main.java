8f38f16ca9568343688ab65cfc3c0d7caf69f5a7
Harutyun Margaryan <harutyun_margaryan@edu.aua.am>
2014-09-19 11:17:38 +0400

    initial commit

---
+++ BuyStore/src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+/**
+ * Created by HARUT1 on 9/17/2014.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+
